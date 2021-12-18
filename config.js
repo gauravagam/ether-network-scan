1170fe2a1cc877c4e96685817c72eeb8cdcd0bdf
Gaurav Agam <gaurav.agam11@gmail.com>
2021-12-18 19:14:33 +0530

    changed provider connection

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    INFURA_URL: "https://ropsten.infura.io/v3/dd35b701f8bf4e6cbdfc5deb1a38fb1d"
+} 
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-import { ethers, providers } from 'ethers';
+import { ethers,providers } from 'ethers';
 import moment from 'moment'
-let provider = new providers.JsonRpcProvider("https://ropsten.infura.io/v3/dd35b701f8bf4e6cbdfc5deb1a38fb1d")
-// console.log('provider ',provider);
+import { INFURA_URL } from './config.js';
+let provider = new providers.JsonRpcProvider(INFURA_URL)
+
 window.onload = async function(){
     let blockNumber = await provider.getBlockNumber();
-    console.log('blockNumber ',blockNumber)
     for(let i=0;i<10;i++){
         const block =  await provider.getBlockWithTransactions(blockNumber-i);
-        // console.log(JSON.stringify(block))
         let tbody = $("#tbody");
         let tr = $("<tr></tr>").appendTo(tbody)
         const blockTime = moment(block.timestamp*1000);
         const currentTime = moment();
         const diff = currentTime.diff(blockTime);
         const age = diff<60000?`${currentTime.diff(blockTime,'seconds')} secs ago`:`${currentTime.diff(blockTime,'minutes')} mins ago`;
-        tr.append(`<td><a href='/block-details.html'>${block.number}</td>`);
+        tr.append(`<td><a href='/block-details.html?blockNumber=${block.number}'>${block.number}</td>`);
         tr.append(`<td>${age}</td>`);
         tr.append(`<td>${block.transactions.length}</td>`);
         tr.append(`<td>${block.miner}</td>`);
